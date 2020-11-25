370d9aab249be5665f66ae9ccd08c2d86b16c8c2
hosam mohamed <hosamelmalt@gmail.com>
2020-11-25 12:07:56 -0800

    Initial Commit

---
+++ CustomActionSheet/CustomActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomActionSheetApp.swift
+//  CustomActionSheet
+//
+//  Created by Hossam on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
